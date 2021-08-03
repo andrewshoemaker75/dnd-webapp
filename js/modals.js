e6223ed9befc58268db47b21ebc0e0fb333884a2
Drew Shoemaker <andrewshoemaker75@gmail.com>
2021-08-02 21:43:01 -0400

    Update modals.js

--- js/modals.js
+++ js/modals.js
@@ -19,38 +19,37 @@ span.onclick = function() {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-
 // FERAL INSTINCT MODAL
 
 // Get the modal
-var modal = document.getElementById("feralInstinctModal");
+var modal2 = document.getElementById("feralInstinctModal");
 
 // Get the button that opens the modal
-var btn = document.getElementById("feralInstinct");
+var btn2 = document.getElementById("feralInstinct");
 
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+var span2 = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
+btn2.onclick = function() {
+  modal2.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+span2.onclick = function() {
+  modal2.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
+  switch (event.target.id) {
+    case 'primalPathModal':
+      modal.style.display="none";
+
+    case 'feralInstinctModal':
+      modal2.style.display="none";
+
+    default:
+      console.log('no match')
   }
 }
